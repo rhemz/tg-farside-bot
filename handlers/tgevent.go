503288da2b10932e899ab0f528631629547add16
unknown <rczeien@gmail.com>
2023-01-28 00:22:24 -0500

    stub out tg endpoint

--- handlers/tgevent.go
+++ handlers/tgevent.go
@@ -1,10 +1,20 @@
 package handlers
 
 import (
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	"github.com/labstack/echo/v4"
 	"net/http"
 )
 
 func (h *Handler) TelegramEventEndpoint(c echo.Context) error {
+	var update tgbotapi.Update
+
+	err := c.Bind(&update)
+	if err != nil {
+		// even if error binding, always return 200 to not trigger telegram infinite redelivery
+		return c.JSONPretty(http.StatusOK, Response{Message: "error reading JSON"}, JsonIndent)
+	}
+
 	return c.JSONPretty(http.StatusOK, Response{Message: "ok"}, JsonIndent)
+
 }
